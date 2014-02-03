0633f54424bb60987a189e86200e3f8c913406bd
luo19930414 <luo19930414@gmail.com>
2014-02-03 01:30:45 -0500

    partially implement urls.py and views.py

--- mmm/mmm/mmm_app/views.py
+++ mmm/mmm/mmm_app/views.py
@@ -1,3 +1,26 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from mmm_app.models import *
 
-# Create your views here.
+def landing(request):
+
+
+def login(request):
+
+
+def profile(request):
+
+
+def settings(request):
+
+
+def new_project(request):
+
+
+def edit_project(request, proj_id):
+
+
+def view_project(request, proj_id):
+
+
+def gallery(request):
