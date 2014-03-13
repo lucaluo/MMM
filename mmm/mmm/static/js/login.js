858b2eec1fd1f0378e7d6abc1557f136f5b82105
HumzaS <humzamsiddiqui@gmail.com>
2014-03-12 21:58:28 -0400

    warning, passwords don't match

--- mmm/mmm/static/js/login.js
+++ mmm/mmm/static/js/login.js
@@ -3,5 +3,14 @@ $(document).ready(function(){
     	window.location.href = "landing.html";
     	alert("why does this only work with an alert?");
     	});
+    	$("#confirmer, #passwordBox").keyup(function() {
+    		if ($("#confirmer").val() != $("#passwordBox").val()){
+    		$(".alert-danger").show();
+    		$("#register").attr("disabled","disabled");
+    		} else {
+    		$(".alert-danger").hide();
+    		$("#register").removeAttr("disabled");
+    		}
+    		});
     	});
     	
