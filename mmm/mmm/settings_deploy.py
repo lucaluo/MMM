9edaf47efd3e23a283d4bd3af5eb3a752237e421
luo19930414 <luo19930414@gmail.com>
2014-04-18 23:34:53 -0400

    set deploy settings

---
+++ mmm/mmm/settings_deploy.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mmmdb',
+        'USER': 'mmmadmin',
+        'PASSWORD': 'mmmadmin',
+        'HOST': 'mmmdb.cdrbqxg7zaha.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'AKIAIVE5YLS74DZHZIWQ'
+EMAIL_HOST_PASSWORD = 'AtufoNvzr4B0NyRyE4I7jjXTpxGiT6M4QOZMRd7tA0QL'
