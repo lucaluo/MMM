46267c334fabceeffe6b27a13b9c2afd3c6abd70
luo19930414 <luo19930414@gmail.com>
2014-04-21 14:44:46 -0400

    fix textFeed

--- mmm/mmm/static/js/landing.js
+++ mmm/mmm/static/js/landing.js
@@ -46,7 +46,7 @@ $('#filterTrigger').click(function(event){
 			break;
     		}
     		$(this).html(newHtml);
-    		$(this).parent().parent().height($(this).parent().height() + $(this).parent().parent().children(".title").height() + 50);
+    		$(this).parent().parent().height($(this).parent().height() + $(this).parent().parent().children(".title").height() + 40);
 		};
 		});
 		
